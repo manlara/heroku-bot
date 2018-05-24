6e6960d7fc3fb502b59f3e0e47d2bd8fbbb7eddc
Manuel Lara <manlara@gmail.com>
2018-05-23 21:05:30 -0500

    first commit

---
+++ config.py
@@ -0,0 +1,7 @@
+# Twitter API Keys
+consumer_key = "VASOMvqr2U6JdoaWG05EF26Dt"
+consumer_secret = "IkenYIItWauGdU7hvKMc64stiOjdpM9bgmMFHbD00e3shgUJB5"
+access_token = "16868544-7xWRqkMzIpKWvHvp5U3fTuHL10H8ZQcHIvBcwbqg6"
+access_token_secret = "o1O8hUmyJkfMBPPhGseR4j3cL69RhhpD3jP6cXJU8DKSU"
+
+
